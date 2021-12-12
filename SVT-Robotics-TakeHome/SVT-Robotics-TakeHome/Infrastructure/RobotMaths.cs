9db1e20da79e68549ce740c57147349a9e104a86
Jack Soulen <jacksoulen@gmail.com>
2021-12-12 15:57:52 -0500

    slight refactoring and adding comments

--- SVT-Robotics-TakeHome/SVT-Robotics-TakeHome/Infrastructure/RobotMaths.cs
+++ SVT-Robotics-TakeHome/SVT-Robotics-TakeHome/Infrastructure/RobotMaths.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Threading.Tasks;
+// Vector2.Distance is better than writing my own distance calc since I get guaranteed best practices
+// But, just for the record, I do know how to do that if needed :)
 
 namespace SVT_Robotics_TakeHome.Infrastructure
 {
@@ -10,23 +10,31 @@ namespace SVT_Robotics_TakeHome.Infrastructure
     {
         public static Robot GetOptimalRobotForLoad(List<Robot> robots, Load load)
         {
+            // Set optimal to any robot
             Robot optimalRobot = robots.First();
-            optimalRobot.Coordinates = new Vector2(optimalRobot.X, optimalRobot.Y);
 
-            float bestDistance;
+            // Use Vector2 for x,y plane maths library
+            optimalRobot.Coordinates = new Vector2(optimalRobot.X, optimalRobot.Y);
+            load.Coordinates = new Vector2(load.X, load.Y);
+
+            float bestDistance = Vector2.Distance(optimalRobot.Coordinates, load.Coordinates);
+
             foreach (Robot currentRobot in robots)
             {
-                currentRobot.Coordinates = new Vector2(currentRobot.X, currentRobot.Y);
-                bestDistance = Vector2.Distance(optimalRobot.Coordinates, load.Coordinates);
+                currentRobot.Coordinates = new Vector2(currentRobot.X, currentRobot.Y);                
                 var currentDistance = Vector2.Distance(currentRobot.Coordinates, load.Coordinates);
-                if (currentDistance < 10)
+
+                // If distances less than 10, choose better battery
+                if (currentDistance < 10 && bestDistance < 10)
                 {
                     if (optimalRobot.BatteryLevel > currentRobot.BatteryLevel) break;
                     else optimalRobot = currentRobot;
                 }
+                // Otherwise if the bestDistance is beat, use the currentRobot
                 else if (currentDistance < bestDistance)
                 {
                     optimalRobot = currentRobot;
+                    bestDistance = currentDistance;
                 }
             }
 
